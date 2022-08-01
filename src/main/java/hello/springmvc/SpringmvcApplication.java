d381e121e281ceb4faac176e3a869b4262ae6562
kimdonghyeon <sh2115521@naver.com>
2022-08-02 00:22:08 +0900

    init

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -3,6 +3,8 @@ package hello.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+//kimdonghyeon tyhmeleaf-basic
 @SpringBootApplication
 public class SpringmvcApplication {
 
